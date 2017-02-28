ff4c7b70a43b3e8723f501892f8c8aa054c9936d
Maria <maria.powell.osu.com>
2017-02-27 17:08:02 -0800

    taking out unneeded module

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,6 @@ var Routes = require('./routes/');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('./config');
 
--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const Routes = require('./routes/');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const config = require('./config');
 
